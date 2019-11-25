fe90390dd054736c4287db80ce495c98df5a4503
joshua richards <jermain119@gmail.com>
2019-11-25 16:43:13 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-   get '/' do
+
+  get '/' do
     erb :user_input
   end
 
-
-  post '/piglantinize' do
-  pl = PigLatinizer.new
+  post '/piglatinize' do
+    pl = PigLatinizer.new
     @piglatin = pl.piglatinize(params[:user_phrase])
     erb :results
   end
-  
+
 end

f4f175eb80b0ecf4130464111b047a41af6ef924
joshua richards <jermain119@gmail.com>
2019-11-25 16:37:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+   get '/' do
+     
+   end 
+  
+  post '/piglantinize' do
+  
+  end
+  
+  
+  post '/piglantinize' do
+  
+  end
+  
 end
